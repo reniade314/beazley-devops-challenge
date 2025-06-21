1aeea42ebedcdb2352d9fdbe1b4ac849a65944e0
tosinade314 <tadebote@gmail.com>
2025-06-21 08:21:50 -0500

    added gitignore

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vmss_names" {
-  value = [
-    azurerm_linux_virtual_machine_scale_set.tiers["web"].name,
-    azurerm_linux_virtual_machine_scale_set.tiers["app"].name,
-    azurerm_linux_virtual_machine_scale_set.tiers["db"].name
-  ]
-}
